9eab82b488fe8fec18ec83487cef610217c208a0
Glitch (hello-webpage) <none>
2020-07-15 18:23:19 +0000

    👕🌯 Checkpoint ./style.css:17675406/64:17675426/125 ./index.html:17675406/1202:17675419/73:17675426/925 ./script.js:17675419/360 ./.glitch-assets:17675406/1112

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
